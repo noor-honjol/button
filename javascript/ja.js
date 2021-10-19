fecc6f3fb409c276e20f40af7e1b53dd98a588cc
noor-honjol <noorhonjol09@gmail.com>
2021-10-19 21:59:12 +0300

    Update ja.js

--- javascript/ja.js
+++ javascript/ja.js
@@ -1,6 +1,14 @@
+let ss=0;
+let bb=['https://c.tenor.com/pSTSsdYLga0AAAAd/you-so-funny-youre-so-funny.gif','https://c.tenor.com/pSTSsdYLga0AAAAd/you-so-funny-youre-so-funny.gif','https://c.tenor.com/pSTSsdYLga0AAAAd/you-so-funny-youre-so-funny.gif']
+
 function presser(){
+    
+    ss++;
     var image=document.createElement('img');
     image.src=('https://c.tenor.com/pSTSsdYLga0AAAAd/you-so-funny-youre-so-funny.gif');
     var div= document.getElementById('flex-box-j');
-    div.append(image);
+    if(ss<7){
+        div.append(image);
+    }
+    
 }
