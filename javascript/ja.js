62fe84c2232e95c9a596738a56c4619781923f06
noor-honjol <noorhonjol09@gmail.com>
2021-10-19 20:58:32 +0300

    button

---
+++ javascript/ja.js
@@ -0,0 +1,6 @@
+function presser(){
+    var image=document.createElement('img');
+    image.src=('https://c.tenor.com/pSTSsdYLga0AAAAd/you-so-funny-youre-so-funny.gif');
+    var div= document.getElementById('flex-box-j');
+    div.append(image);
+}
